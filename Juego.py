6bf2d9fa75ab790dcb0cb3f568e6bd361d754d15
jultrun <jultrun121@hotmail.com>
2014-11-03 22:08:21 -0500

    a

--- Juego.py
+++ Juego.py
@@ -9,7 +9,7 @@ from __builtin__ import str
 # variables
 score=0
 nivel=1
-vidas=10
+vidas=14343
 # Constantes
 WIDTH = 640
 HEIGHT = 480
@@ -23,15 +23,12 @@ class NavePanel(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left = x
         self.rect.top = y
-    def disparar(self,superficie,mouse):  
-        if mouse==(1,0,0): #se haga clik dibujar 2 lineas saliendo desde los extremos inferiores hasta la mira
-            laser=pygame.mixer.Sound("data/LASER1.WAV")
-            laser.play()
-            pygame.draw.line(superficie,COLOR_LASER,(0,HEIGHT),pygame.mouse.get_pos(),4)
-            pygame.draw.line(superficie,COLOR_LASER,(WIDTH,HEIGHT),pygame.mouse.get_pos(),4)   
-            return True 
-        else:
-            return False      
+    def disparar(self,superficie,objetivo):      
+        pygame.draw.line(superficie,COLOR_LASER,(0,HEIGHT),pygame.mouse.get_pos(),4)
+        pygame.draw.line(superficie,COLOR_LASER,(WIDTH,HEIGHT),pygame.mouse.get_pos(),4)  
+        laser=pygame.mixer.Sound("data/LASER1.WAV")
+        laser.play() 
+        objetivo.destruir()
     def update(self, superficie):
         superficie.blit(self.image,self.rect) 
 class Mira(pygame.sprite.Sprite):
@@ -52,7 +49,7 @@ class Asteriode(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.centery = y
-    def mover(self,time):
+    def mover(self):
         self.rect.move_ip(0,2)
     def update(self,superficie):
         superficie.blit(self.image,self.rect)      
@@ -64,7 +61,7 @@ class waveAsteorides(): #crea un arreglo de asteroides y los mueve y actualia
     def cwave(self):
         global nivel
         nivel = round(self.numWavesDestroy/10)+1#nivel aumenta cada 10 oleadeas(WAVEs) destruidos por uno mismo
-        astMin,astMax=({1:(2,6), 2:(3,7), 3:(4,8)}[nivel])#ajusta el  minimo y maximo de asteorides de la oleada por cada nivel
+        astMin,astMax=({1:(2,6), 2:(3,7), 3:(4,8),4:(0,0)}[nivel])#ajusta el  minimo y maximo de asteorides de la oleada por cada nivel
         if len(self.wave)==0:
             num=random.randint(astMin,astMax)
             while(len(self.wave)<num):
@@ -72,9 +69,9 @@ class waveAsteorides(): #crea un arreglo de asteroides y los mueve y actualia
                 numeroy= random.randint(-3*self.AstroWidh,-self.AstroWidh)
                 self.wave.append(Asteriode(numerox,numeroy))
             return self.wave
-    def mover(self,time):
+    def mover(self):
         for asteriodes in self.wave: #por cada asteroidede la wavea llamar la funcion mover
-            asteriodes.mover(time)
+            asteriodes.mover()
             if asteriodes.rect.centery >= (HEIGHT): #si esta por debajo del mapo se borra
                 self.wave.remove(asteriodes)
                 global vidas
@@ -110,6 +107,7 @@ def load_image(filename, transparent=False):
  
 def main():
     #iniciar variables
+    playing=True
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Viaje a la Galxia Negra")
     pygame.mouse.set_visible(False)
@@ -119,19 +117,17 @@ def main():
     fuenteDig=pygame.font.Font("data/fuenteDigital.TTF",20)#ds digital datafont.com
     fondo = pygame.image.load("data/Space.jpg")#flick
     clock = pygame.time.Clock()
-    while True:   
+    while playing:   
             time = clock.tick(60)
             screen.blit(fondo,(0,0))
             astro.cwave()                 
             for eventos in pygame.event.get():
                 if eventos.type == QUIT:
-                    pygame.quit()
-                    sys.exit()           
+                    playing=False           
                 if eventos.type == MOUSEBUTTONDOWN:
-                    if navePanel.disparar(screen, pygame.mouse.get_pressed()):
-                        astro.destruir() 
+                    navePanel.disparar(screen, astro)                 
             #UPDATES/DIBIJAR
-            astro.mover(time)
+            astro.mover()
             astro.update(screen)
             mira.mover(pygame.mouse.get_pos())
             mira.update(screen)            
@@ -142,24 +138,25 @@ def main():
             screen.blit(textoScore,(400,HEIGHT-35))
             screen.blit(textoVidas,(200,HEIGHT-35))         
             pygame.display.update()
+            print "fps:", clock.get_fps()
             #fin del juego
-            #if vidas <=0 or astro.waveExis():
-             #   fuenteEnd=pygame.font.Font("data/fuenteDigital.TTF",80)
-              #  pygame.mouse.set_visible(True)
-               # while True:
-                #    screen.blit(fondo,(0,0))
-                 #   msj=""
-                  #  if vidas <=0:
-                   #     msj="Has perdido"
-                    #elif astro.waveExis():
-                     #   msj="has ganado"
-                    #textoEnd=fuenteEnd.render(msj,0,(0,225,0))
-                   # screen.blit(textoEnd,( (WIDTH/2)-fuenteEnd.get_height() ,(HEIGHT/2)-fuenteEnd.get_height() ) )
-                  #  for eventos in pygame.event.get():
-                   #     if eventos.type == QUIT:
-                    #        pygame.quit()
-                     #       sys.exit()
-                   # pygame.display.update()                 
+            if vidas <=0 or nivel>=4:
+                gameover=True
+                while gameover:
+                    for eventos in pygame.event.get():
+                        if eventos.type == QUIT:
+                            playing=gameover=False 
+                    pygame.mouse.set_visible(True)
+                    screen.blit(fondo,(0,0))
+                    if vidas <=0:
+                        msj="Has perdido"
+                    else:
+                        msj="has ganado"
+                    textoEnd=fuenteDig.render(msj,0,(0,225,0))
+                    screen.blit(textoEnd,( (WIDTH/2)-fuenteDig.get_height() ,(HEIGHT/2)-fuenteDig.get_height() ) )               
+                    pygame.display.update()
+    pygame.quit()
+    sys.exit()                
     return 0
  
 if __name__ == '__main__':
